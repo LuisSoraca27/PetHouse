4f71c34dc382ff54181014ae7a02fdcc03ef86b7
LuisSoraca27 <luisenriquesoraca27@gmail.com>
2023-01-22 10:06:21 -0500

    video page agree

---
+++ src/components/Home_logIn.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Home_logIn = () => {
+    return (
+        <LogIn>
+            
+        </LogIn>
+    );
+};
+
+export default Home_logIn;
+
+const LogIn = styled.div`
+width: 50%;
+height: 100%;
+background-color: red;
+`
---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import video from '../assets/video.mp4'
+
+const Home = () => {
+    return (
+
+        <ContainerHome>
+            <video src={video} autoplay="true" muted="true" loop="true" >
+            </video>
+        </ContainerHome>
+
+    );
+};
+
+export default Home;
+
+
+const ContainerHome = styled.div`
+width: 100vw;
+height: 100vh;
+display: flex;
+
+video {
+    position: fixed;
+    min-height: 100%;
+    min-width: 100%;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+}
+`
+
